96363005f592f997c7f2ee6cf2e86db91fad7bec
Alex Nitta <a.j.nitta@gmail.com>
2016-04-02 17:58:07 -0700

    Complete 6ees assignment

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,13 @@
 import Grub from './Grub'
 
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor() {
+    super()
+    this.job = 'Keep on growing'
+    this.age = 5
+    this.color = 'yellow'
+  }
 };
 
 export default Bee;
